ffda02ca3e98cf5d523a28d7b1feb734471f753e
Edunno <tamericandream@hotmail.com>
2019-03-12 12:12:11 +0100

    Hobby Entity

---
+++ CA2/src/main/java/entity/Hobby.java
@@ -0,0 +1,83 @@
+/* Esben Dalgaard; DECK-CS */
+
+package entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+/**
+ * @author Esben
+ * All rights belong to respective contributors.
+ */
+@Entity
+public class Hobby implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    @ManyToMany(mappedBy = "hobbies", cascade = CascadeType.ALL)
+    private List<Person> persons = new ArrayList();
+
+    public Hobby() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Person> getPersons() {
+        return persons;
+    }
+
+    public void addPersons(Person p) {
+        this.persons.add(p);
+    }
+    
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Hobby)) {
+            return false;
+        }
+        Hobby other = (Hobby) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Hobby[ id=" + id + " ]";
+    }
+
+}
