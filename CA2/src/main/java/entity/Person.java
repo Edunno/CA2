a41ae93bd0eff58ea3b7d586da3290586e05b6d7
Edunno <tamericandream@hotmail.com>
2019-03-12 11:29:30 +0100

    Person entity

--- CA2/src/main/java/entity/Person.java
+++ CA2/src/main/java/entity/Person.java
@@ -17,9 +17,40 @@ public class Person implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    private String email;
+    private String firstName;
+    private String lastName;
 
+    public Person() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    
     public Integer getId() {
         return id;
     }
