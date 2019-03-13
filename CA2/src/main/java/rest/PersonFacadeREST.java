0ee8f90ee4dd3b0c306aadae1133a51e11220981
Marx02 <casper_marx@msn.com>
2019-03-13 11:22:54 +0100

    Edits in personREST, still not working

--- CA2/src/main/java/rest/PersonFacadeREST.java
+++ CA2/src/main/java/rest/PersonFacadeREST.java
@@ -24,22 +24,24 @@ import javax.ws.rs.core.Response;
 import utils.DBFacade;
 import com.google.gson.Gson;
 import java.util.ArrayList;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
 
 /**
  *
  * @author caspe
  */
 @Path("person")
-public class PersonFacadeREST extends AbstractFacade<Person> {
+public class PersonFacadeREST {
 
     Gson gson = new Gson();
     DBFacade dbf = new DBFacade();
+    @Context
+    private UriInfo context;
 
-    @PersistenceContext(unitName = "CA2DB")
-    private EntityManager em;
+    
 
     public PersonFacadeREST() {
-        super(Person.class);
     }
 
 //    @GET
@@ -72,57 +74,6 @@ public class PersonFacadeREST extends AbstractFacade<Person> {
         return Response.ok().entity(gson.toJson(p)).build();
     }
 
-    @POST
-    @Override
-    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public void create(Person entity) {
-        super.create(entity);
-    }
 
-    @PUT
-    @Path("{id}")
-    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public void edit(@PathParam("id") Integer id, Person entity) {
-        super.edit(entity);
-    }
-
-    @DELETE
-    @Path("{id}")
-    public void remove(@PathParam("id") Integer id) {
-        super.remove(super.find(id));
-    }
-
-    @GET
-    @Path("{id}")
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public Person find(@PathParam("id") Integer id) {
-        return super.find(id);
-    }
-
-    @GET
-    @Override
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public List<Person> findAll() {
-        return super.findAll();
-    }
-
-    @GET
-    @Path("{from}/{to}")
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public List<Person> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
-        return super.findRange(new int[]{from, to});
-    }
-
-    @GET
-    @Path("count")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String countREST() {
-        return String.valueOf(super.count());
-    }
-
-    @Override
-    protected EntityManager getEntityManager() {
-        return em;
-    }
 
 }
